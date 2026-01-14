811311e78bcc5ffd88e87a67796a6405f864d390
diedromeo <119026435+diedromeo@users.noreply.github.com>
2026-01-14 09:08:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1009,4 +1009,4 @@ CLOUDFLARE_BLOCK_TEMPLATE = """
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=9888)
